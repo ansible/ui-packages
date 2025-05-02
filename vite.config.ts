c136583597f6a2b470f152890fde7e0a3d9883b0
James Talton <talton@gmail.com>
2025-05-02 09:58:13 -0400

    fix: vite-plugin-externalize-deps
    Signed-off-by: James Talton <talton@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,23 @@ import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 import { defineConfig, UserConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@module-federation/enhanced',
-        '@patternfly/react-core',
-        '@patternfly/react-icons',
-        'react',
-        'react-dom',
-        'react-router',
-      ],
-      output: {
-        globals: {
-          '@module-federation/enhanced': 'ModuleFederationEnhanced',
-          '@patternfly/react-core': 'PatternFlyReactCore',
-          '@patternfly/react-icons': 'PatternFlyReactIcons',
-          'react-dom': 'ReactDom',
-          'react-router': 'ReactRouter',
-          react: 'React',
-        },
-      },
-    },
-  },
+  plugins: [
+    react(),
+    dts(),
+    externalizeDeps({
+      deps: false,
+      devDeps: false,
+      except: [],
+      nodeBuiltins: false,
+      optionalDeps: true,
+      peerDeps: true,
+      useFile: path.join(process.cwd(), 'package.json'),
+    }),
+  ],
   esbuild: { legalComments: 'none' },
   test: {
     environment: 'happy-dom',
